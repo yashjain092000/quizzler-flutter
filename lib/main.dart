d7ea4012f0e211317a7d5c3c1f9d7e5eccbafa62
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-09 17:42:53 -0500

    declare a new list of type bool and add the answers of the questios respectively

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.'
   ];
+  List<bool> answers =[false,true,true];
   int questionNo=0;
   /*
   question1: 'You can lead a cow down stairs but not up stairs.', false,
